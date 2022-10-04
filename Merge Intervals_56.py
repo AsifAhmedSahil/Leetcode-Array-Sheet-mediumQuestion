0d7f46f48ae2229cbc9e749c541fdca718eeca09
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-10-04 20:54:12 +0600

    first commit

---
+++ Merge Intervals_56.py
@@ -0,0 +1,4 @@
+'''
+1st kaj --> sort korbo,then oi sort er 1st index k output nibo...then 2nd theke last porjonto loop krbo, 
+most recent tupple er last index er value nibo ..then oita k compare krbo jeta te achi oitar 1st er sathe ,jodi choto hoi tahole interval hoise,nahole output e push kore dibo tupple ta k...then return output.
+'''
